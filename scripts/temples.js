003dd67c414b1b98678944b34e69a80be596c6ef
Mateus de Sousa Schiavi <mtsschiavi@gmail.com>
2026-01-17 02:30:46 -0300

    burger button added

---
+++ scripts/temples.js
@@ -0,0 +1,8 @@
+const menuToggle = document.getElementById('menu-toggle');
+const navMenu = document.getElementById('nav-menu');
+
+menuToggle.addEventListener('click', () => {
+    navMenu.classList.toggle('open');
+
+    menuToggle.textContent = navMenu.classList.contains('open') ? 'x' : '☰';
+})
